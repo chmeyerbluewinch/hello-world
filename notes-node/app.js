160a05566fa96c0b7d80b4d310188356be92a2bc
osboxes <osboxes@osboxes>
2018-09-09 22:46:01 +0200

    modules added

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,8 +3,15 @@ console.log("Starting app.js")
 const fs = require("fs");
 const os = require("os");
 const notes = require("./notes.js");
+const _ = require("lodash");
 
 console.log("Result: ", notes.add(9, -2));
+
+console.log(_.isString(true));
+console.log(_.isString("Hallo"));
+
+var filteredArray =_.uniq(["Christoph", 1, 2, "Christoph", 1, 3, 4, 5]);
+console.log(filteredArray);
 
 
 //var user = os.userInfo();

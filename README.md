e4f7c01adfeac6700c9bb6fbf8b2b3a022ab969d
osboxes <osboxes@osboxes>
2018-09-09 20:27:02 +0200

    weiterer Test

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ synchronized: 09.09.2018, 18:30, cme
 synchronized: 09.09.2018, 18:40, cme
 
 some lines removed: 09.09.2018, 18:46, cme
+
+noch eine Anpassung: 09.09.2018, 19:12, cme
--- notes-node/app.js
+++ notes-node/app.js
@@ -6,6 +6,7 @@ const notes = require("./notes.js");
 
 console.log("Result: ", notes.add(9, -2));
 
+
 //var user = os.userInfo();
 //
 //// fs.appendFile("greetings.txt", "Hello " + user.username + "!\n", function (err) {
